4bb6797229465497eb2cdb11847fc98ca9b7ceb3
myothuko98 <myothuko@ucsy.edu.mm>
2022-07-27 13:11:18 +0630

    add redux persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,24 +1,25 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
-
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { rootReducer } from "./root-reducer";
 
-// const loggerMiddleware = (store) => (next) => (action) => {
-//   if (!action.type) {
-//     return next(action);
-//   }
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
 
-//   console.log("type :", action.type);
-//   console.log("payload :", action.payload);
-//   console.log("current state :", store.getState());
-
-//   next(action);
-
-//   console.log("new state :", store.getState());
-// };
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleWares = [logger];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);

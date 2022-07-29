ff11cbf3133b0a937af4b54eec65ca79f16b9263
myothuko98 <myothuko@ucsy.edu.mm>
2022-07-29 16:42:15 +0630

    finish implemented redux-thunk

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,14 +1,19 @@
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { selectCategoriesMap } from "../../store/categories/category.selector";
+import {
+  selectCategoriesIsLoading,
+  selectCategoriesMap,
+} from "../../store/categories/category.selector";
 import ProductCard from "../../components/product-card/product-card.component";
 import "./category.styles.jsx";
 import { CategoryContainer, CategoryTitle } from "./category.styles.jsx";
+import Spinner from "../../components/spinner/spinner.component";
 
 const Category = () => {
   const { category } = useParams();
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
 
   const [products, setProducts] = useState(categoriesMap[category]);
   useEffect(() => {
@@ -16,15 +21,19 @@ const Category = () => {
   }, [category, categoriesMap]);
 
   return (
-    <>
+    <Fragment>
       <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
-      <CategoryContainer>
-        {products &&
-          products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-      </CategoryContainer>
-    </>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <CategoryContainer>
+          {products &&
+            products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+        </CategoryContainer>
+      )}
+    </Fragment>
   );
 };
 
--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -17,3 +17,9 @@ export const selectCategoriesMap = createSelector(
       return acc;
     }, {})
 );
+
+export const selectCategoriesIsLoading = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.isLoading
+)
+
